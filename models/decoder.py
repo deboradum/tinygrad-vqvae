8d4284baa424a258a247061d74b792b83acbccc4
deboradum <pvwpvw@protonmail.com>
2025-04-01 20:06:37 +0200

    encoder + decoder work

--- models/decoder.py
+++ models/decoder.py
@@ -1,9 +1,20 @@
-import mlx.nn as nn
+import tinygrad.nn as nn
+from residual import ResidualStack
 
 
-class Decoder(nn.Module):
-    def __init__(self):
-        return
+class Decoder():
+    def __init__(self, in_dim, h_dim, res_h_dim, num_res_layers):
+        self.inv_conv_layers = [
+            nn.ConvTranspose2d(in_dim, h_dim, kernel_size=3, stride=1, padding=1),
+            ResidualStack(h_dim, h_dim, res_h_dim, num_res_layers),
+            nn.ConvTranspose2d(h_dim, h_dim // 2, kernel_size=4, stride=2, padding=1),
+            nn.Conv2d(h_dim // 2, 3, kernel_size=3, stride=1, padding=1),
+        ]
 
     def __call__(self, x):
-        return x
+        for layer in self.inv_conv_layers[:-1]:
+            x = layer(x)
+        x = x.relu()
+        x = self.inv_conv_layers[-1](x)
+
+        return self.res(x)
--- models/encoder.py
+++ models/encoder.py
@@ -1,19 +1,20 @@
 import tinygrad.nn as nn
+
 from residual import ResidualStack
 
 
-class Encoder(nn.Module):
-    def __init__(self, in_dim, h_dim, k, d):
+class Encoder():
+    def __init__(self, in_dim, h_dim, res_h_dim, num_res_layers):
         self.conv_layers = [
             nn.Conv2d(in_dim, h_dim // 2, kernel_size=4, stride=2, padding=1),
             nn.Conv2d(h_dim // 2, h_dim, kernel_size=4, stride=2, padding=1),
             nn.Conv2d(h_dim, h_dim, kernel_size=3, stride=1, padding=1),
         ]
-        self.res = ResidualStack(h_dim, )
+        self.res = ResidualStack(h_dim, h_dim, res_h_dim, num_res_layers)
 
     def __call__(self, x):
-        for layer in self.layers[:-1]:
+        for layer in self.conv_layers[:-1]:
             x = layer(x).relu()
-        x = self.layers[-1](x)
+        x = self.conv_layers[-1](x)
 
         return self.res(x)
