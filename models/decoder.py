83f747da92207108007bbb062a8ce01667e64017
deboradum <pvwpvw@protonmail.com>
2025-04-01 21:11:45 +0200

    fix decoder bug

--- models/decoder.py
+++ models/decoder.py
@@ -8,7 +8,7 @@ class Decoder():
             nn.ConvTranspose2d(in_dim, h_dim, kernel_size=3, stride=1, padding=1),
             ResidualStack(h_dim, h_dim, res_h_dim, num_res_layers),
             nn.ConvTranspose2d(h_dim, h_dim // 2, kernel_size=4, stride=2, padding=1),
-            nn.Conv2d(h_dim // 2, 3, kernel_size=3, stride=1, padding=1),
+            nn.ConvTranspose2d(h_dim // 2, 3, kernel_size=4, stride=2, padding=1),
         ]
 
     def __call__(self, x):
@@ -17,4 +17,4 @@ class Decoder():
         x = x.relu()
         x = self.inv_conv_layers[-1](x)
 
-        return self.res(x)
+        return x
