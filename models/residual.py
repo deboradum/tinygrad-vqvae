ce1076da137d9d2b724cb0a73052e61689f85d5a
deboradum <pvwpvw@protonmail.com>
2025-04-04 22:36:08 +0200

    try other residual format

--- models/residual.py
+++ models/residual.py
@@ -3,11 +3,16 @@ import tinygrad.nn as nn
 
 class ResidualLayer():
     def __init__(self, in_dim, out_dim, h_dim):
-        self.conv1 = nn.Conv2d(in_dim, h_dim, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(h_dim, out_dim, kernel_size=1, stride=1, bias=False)
+        self.conv1 = nn.Conv2d(in_dim, h_dim, kernel_size=3, stride=1, padding=1, bias=True)
+        self.conv2 = nn.Conv2d(h_dim, out_dim, kernel_size=1, stride=1, bias=True)
 
     def __call__(self, x):
-        return x + self.conv2(self.conv1(x.relu()).relu())
+        x_ = x.relu()
+        x_ = self.conv1(x_)
+        x_ = x_.relu()
+        x_ = self.conv2(x_)
+
+        return x + x_
 
 class ResidualStack():
     def __init__(self, in_dim, out_dim, h_dim, num_layers):
